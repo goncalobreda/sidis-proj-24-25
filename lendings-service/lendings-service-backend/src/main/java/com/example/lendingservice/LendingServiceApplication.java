192c0ef0240e4dba7aee13ea062db0d9fc603ddf
breda <1221307@isep.ipp.pt>
2024-10-11 16:01:23 +0100

    lendings

---
+++ lendings-service/lendings-service-backend/src/main/java/com/example/lendingservice/LendingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.lendingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LendingServiceApplication.class, args);
+    }
+}
