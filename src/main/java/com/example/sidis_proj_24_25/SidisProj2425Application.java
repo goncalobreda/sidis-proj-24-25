c9fbadbba6e6183bcdae0365751abe7c6f4947ef
breda <1221307@isep.ipp.pt>
2024-10-11 14:30:56 +0100

    test

---
+++ src/main/java/com/example/sidis_proj_24_25/SidisProj2425Application.java
@@ -0,0 +1,13 @@
+package com.example.sidis_proj_24_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidisProj2425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidisProj2425Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sidis_proj_24_25/SidisProj2425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidis_proj_24_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidisProj2425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
